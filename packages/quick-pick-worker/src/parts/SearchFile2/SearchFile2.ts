640b4cd71c04dbb47fbba89901e394da4d1f383a
Le Vivilet <levivilet@pm.me>
2026-02-26 11:47:21 +0100

    refactor: simplify searchFile2 implementation by using FileSearchWorker (#3)

--- packages/quick-pick-worker/src/parts/SearchFile2/SearchFile2.ts
+++ packages/quick-pick-worker/src/parts/SearchFile2/SearchFile2.ts
@@ -1,13 +1,6 @@
-import * as GetProtocol from '../GetProtocol/GetProtocol.ts'
-import * as SearchFileModule from '../SearchFileModule/SearchFileModule.ts'
+import { FileSearchWorker } from '@lvce-editor/rpc-registry'
 
 export const searchFile2 = async (path: string, value: string, prepare: boolean, assetDir: string): Promise<readonly string[]> => {
-  const protocol = GetProtocol.getProtocol(path)
-  // TODO call different providers depending on protocol
-  const fn = SearchFileModule.getFn(protocol)
-  if (!fn) {
-    throw new Error(`No search handler registered for protocol: ${protocol}`)
-  }
-  const result = await fn(path, value, prepare, assetDir)
+  const result = await FileSearchWorker.invoke('FileSearch.searchFile', path, value, prepare, assetDir)
   return result
 }
