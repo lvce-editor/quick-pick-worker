82b83a114d76a0b07f177b601308e35901aeac5e
Le Vivilet <levivilet@pm.me>
2026-03-23 22:30:49 +0100

    fix: correct type in renderItemsDom and update renderIncremental logic

--- packages/quick-pick-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/quick-pick-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,9 +1,10 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 import * as CreateQuickPickViewModel from '../CreateQuickPickViewModel/CreateQuickPickViewModel.ts'
 import * as GetQuickPickVirtualDom from '../GetQuickPickVirtualDom/GetQuickPickVirtualDom.ts'
 
-export const renderItemsDom = (state: QuickPickState): readonly unknown[] => {
+export const renderItemsDom = (state: QuickPickState): readonly VirtualDomNode[] => {
   const viewModel = CreateQuickPickViewModel.createQuickPickViewModel(state, state)
   const { scrollBarHeight, scrollBarTop, visibleItems } = viewModel
   return GetQuickPickVirtualDom.getQuickPickVirtualDom(visibleItems, scrollBarHeight, scrollBarTop)
