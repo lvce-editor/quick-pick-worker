ff368a5d8526c01945ebf334f7ae617b6322d53f
Le Vivilet <levivilet@pm.me>
2026-04-15 17:11:36 +0200

    feat: integrate file search worker and refactor search functionality

--- packages/quick-pick-worker/src/parts/SearchFile2/SearchFile2.ts
+++
@@ -1,6 +0,0 @@
-import { FileSearchWorker } from '@lvce-editor/rpc-registry'
-
-export const searchFile2 = async (path: string, value: string, prepare: boolean, assetDir: string): Promise<readonly string[]> => {
-  const result = await FileSearchWorker.invoke('FileSearch.searchFile', path, value, prepare, assetDir)
-  return result
-}
