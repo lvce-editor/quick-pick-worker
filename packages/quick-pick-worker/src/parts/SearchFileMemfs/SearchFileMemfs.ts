0c015ba562b540d74b9f18d1e75a904a71f9e2a7
Le Vivilet <levivilet@pm.me>
2026-04-17 13:59:57 +0200

    feature: remove deprecated search file handlers and related processes

--- packages/quick-pick-worker/src/parts/SearchFileMemfs/SearchFileMemfs.ts
+++
@@ -1,5 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const searchFile = async (uri: string): Promise<readonly string[]> => {
-  return RendererWorker.invoke('ExtensionHost.searchFileWithMemory', uri)
-}
--- packages/quick-pick-worker/src/parts/SearchFileWithFetch/SearchFileWithFetch.ts
+++
@@ -1,8 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-// TODO simplify code
-// 1. don't have playground prefix in fileMap json
-// 2. remove code here that removes the prefix
-export const searchFile = async (path: string): Promise<readonly string[]> => {
-  return RendererWorker.invoke('ExtensionHost.searchFileWithFetch', path)
-}
