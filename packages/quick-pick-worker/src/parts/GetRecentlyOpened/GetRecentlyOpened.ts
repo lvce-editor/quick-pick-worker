dd02800e103c11bcde909b7cd02fd389077fbbf1
Le Vivilet <levivilet@pm.me>
2026-04-30 11:24:45 +0200

    simplify

--- packages/quick-pick-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/quick-pick-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,5 +1,8 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
-import uniq from 'lodash-es/uniq.js'
+
+const uniq = (array: readonly string[]): readonly string[] => {
+  return [...new Set(array)]
+}
 
 export const getRecentlyOpened = async (): Promise<readonly string[]> => {
   const recentlyOpened = (await RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')) as readonly string[]
