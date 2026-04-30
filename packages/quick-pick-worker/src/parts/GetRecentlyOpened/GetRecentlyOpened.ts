db250d1c32b028c97b02ebf931c6b33f6c0ce0d5
Le Vivilet <levivilet@pm.me>
2026-04-30 11:07:11 +0200

    feature: add lodash-es dependency and implement getRecentlyOpened function with duplicate filtering

--- packages/quick-pick-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
+++ packages/quick-pick-worker/src/parts/GetRecentlyOpened/GetRecentlyOpened.ts
@@ -1,5 +1,7 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import uniq from 'lodash-es/uniq.js'
 
-export const getRecentlyOpened = (): Promise<readonly string[]> => {
-  return RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')
+export const getRecentlyOpened = async (): Promise<readonly string[]> => {
+  const recentlyOpened = (await RendererWorker.invoke(/* RecentlyOpened.getRecentlyOpened */ 'RecentlyOpened.getRecentlyOpened')) as readonly string[]
+  return uniq(recentlyOpened)
 }
