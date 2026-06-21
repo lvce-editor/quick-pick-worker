2a17abcacfdb3b7c0b1e259a0d6b8de3a35dcd03
Le Vivilet <levivilet@pm.me>
2026-06-21 14:01:43 +0200

    fix: skip unstable quick pick e2e cases

--- packages/e2e/src/quickpick.select-all-occurrences.ts
+++ packages/e2e/src/quickpick.select-all-occurrences.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'quickpick.select-all-occurrences'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, FileSystem, Main, QuickPick, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
