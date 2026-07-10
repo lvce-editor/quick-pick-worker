dc2f2bed5f66a3787bef5087418eaf266fa3c1b1
Le Vivilet <levivilet@pm.me>
2026-07-10 21:36:28 +0000

    fix: dispatch escape in quick pick e2e

--- packages/e2e/src/quickpick.keyboard-escape.ts
+++ packages/e2e/src/quickpick.keyboard-escape.ts
@@ -2,12 +2,17 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'quickpick.keyboard-escape'
 
-export const test: Test = async ({ expect, KeyBoard, Locator, QuickPick }) => {
+export const test: Test = async ({ expect, Locator, QuickPick }) => {
   await QuickPick.open()
   const quickPick = Locator('#QuickPick')
   await expect(quickPick).toBeVisible()
 
-  await KeyBoard.press('Escape')
+  const quickPickInput = Locator('#QuickPick .InputBox')
+  await quickPickInput.dispatchEvent('keydown', {
+    bubbles: true,
+    code: 'Escape',
+    key: 'Escape',
+  } as unknown as string)
 
   await expect(quickPick).toBeHidden()
 }
