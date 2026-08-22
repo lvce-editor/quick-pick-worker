1c8a4df824e05137dff503d1449b852569b950ed
Le Vivilet <levivilet@pm.me>
2026-08-22 08:44:54 +0000

    fix: keep command queue within memory budget

--- packages/quick-pick-worker/src/parts/QuickPickStates/QuickPickStates.ts
+++ packages/quick-pick-worker/src/parts/QuickPickStates/QuickPickStates.ts
@@ -1,5 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { QuickPickState } from '../QuickPickState/QuickPickState.ts'
 
-export const { dispose, get, getCommandIds, registerCommands, set, wrapAsyncCommand, wrapCommand, wrapSerialAsyncCommand, wrapSerialCommand } =
-  ViewletRegistry.create<QuickPickState>()
+export const { dispose, get, getCommandIds, registerCommands, set, wrapAsyncCommand, wrapCommand } = ViewletRegistry.create<QuickPickState>()
